09648d1d6bdb0d1fafa9890b2516aef96f5f7d81
Yury V. Zaytsev <yury@shurup.com>
2011-09-26 18:28:39 +0200

    classes: added the initial service class

---
+++ manifests/classes/services.pp
@@ -0,0 +1,61 @@
+# ZYV
+
+#
+# Packages installed by default
+#
+class services::packages {
+
+    $packages_to_install = [
+        'git',
+        'logwatch',
+        'mc',
+        'screen',
+    ]
+
+    package { $packages_to_install :
+        ensure => 'present',
+    }
+
+}
+
+#
+# Services that should be enabled by default
+#
+#class services::enabled {
+#
+#    $services_to_enable = [
+#    ]
+#
+#    service { $services_to_enable :
+#        enable => 'true',
+#        ensure => 'running',
+#    }
+#
+#}
+
+#
+# Services that should be disabled by default
+#
+class services::disabled {
+
+    $services_to_disable = [
+        'abrtd',
+        'avahi-daemon',
+        'iscsi',
+        'iscsid',
+        'kdump',
+        'netfs',
+        'nfs',
+        'nfslock',
+        'rpcbind',
+        'rpcgssd',
+        'rpcidmapd',
+        'sysstat',
+    ]
+
+    service { $services_to_disable :
+        enable => 'false',
+        ensure => 'stopped',
+    }
+
+}
