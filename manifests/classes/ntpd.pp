482c893cf377820be32875ede735b67d33b7b713
Yury V. Zaytsev <yury@shurup.com>
2011-10-04 01:10:39 +0200

    ntpd: only bind to localhost

--- manifests/classes/ntpd.pp
+++ manifests/classes/ntpd.pp
@@ -9,6 +9,13 @@ class services::ntpd {
         ensure => 'present',
     }
 
+    file { '/etc/sysconfig/ntpd':
+        ensure => 'file',
+        notify => Service['ntpd'],
+        require => Package['ntp'],
+        source => "${infra_files}/sysconfig/ntpd",
+    }
+
     service { 'ntpd':
         enable => 'true',
         ensure => 'running',
