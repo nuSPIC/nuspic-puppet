112371e880b75c8bfe5f56583b9f726eb56f8dc2
Yury V. Zaytsev <yury@shurup.com>
2011-10-04 02:01:52 +0200

    nginx: fix non-working 404 page

--- manifests/classes/nginx.pp
+++ manifests/classes/nginx.pp
@@ -19,23 +19,35 @@ class nginx::install {
 class nginx::config {
 
     File {
-        notify => Class['nginx::service'],
         require => Class['nginx::install'],
     }
 
     file { '/etc/nginx/nginx.conf':
         ensure => 'file',
         source => "${infra_files}/nginx/nginx.conf",
+        notify => Class['nginx::service'],
     }
 
     file { '/etc/nginx/conf.d/default.conf':
         ensure => 'file',
         source => "${infra_files}/nginx/conf.d/default.conf",
+        notify => Class['nginx::service'],
     }
 
     file { '/etc/nginx/conf.d/htpasswd-admin':
         ensure => 'file',
         source => "${infra_files}/nginx/conf.d/htpasswd-admin",
+        notify => Class['nginx::service'],
+    }
+
+    file { '/etc/nginx/misc':
+        ensure => 'directory',
+    }
+
+    file { '/etc/nginx/misc/404.html':
+        ensure => 'file',
+        source => "${infra_files}/nginx/misc/404.html",
+        require => File['/etc/nginx/misc'],
     }
 
 }
