92bc67c946f8bfdb5f06c126c2571fc95957ef78
Yury V. Zaytsev <yury@shurup.com>
2013-11-15 16:26:41 +0100

    nuspic: removed python-cjson dependency

--- manifests/classes/nuspic.pp
+++ manifests/classes/nuspic.pp
@@ -16,7 +16,6 @@ class nuspic::install {
     $packages_nuspic = [
         'MySQL-python',
         'python-anyjson',
-        'python-cjson',
         'python-simplejson',
         'python-django',
         'python-django-bbmarkup',
